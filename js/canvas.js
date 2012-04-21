5e9c56b1aa24fe48cf0a87dc4c0ee7401002b46d
Andrew Scagnelli <ascagnel@gmail.com>
2012-04-20 23:10:43 -0700

    Input is working properly now.

--- js/canvas.js
+++ js/canvas.js
@@ -7,94 +7,69 @@
 */
 var drawObject= null;
 var paused= false;
-var stop= false;
 var tick= 0;
 var inputKey= 0;
 var prevTime= 0;
-var down= [];
-var up= [];
 var keys= [];
 var interval;
 
-// development-only vars
-var maxruns= 5;
-var runs= 0;
-
-function init() {
-    msg('in init');
-    $(document).keydown(function(event) {
-        //msg('down: {' + event.keyCode + '}');
-        down[length]= event.keyCode;
-    });
-    $(document).keyup(function(event) {
-        //msg('up: {' + event.keyCode + '}');
-        up[length]= event.keyCode;
-    });
-
-    drawObject = { "x": "0", "y": "0", "color": "blue"};
-    paused= false;
-    stop= false;
-    tick= 50;
-    msg(tick);
-    prevTime= +new Date();
-
-    interval= setInterval("loop()", 0);
-    msg('interval: {'+interval+'}');
-    //loop();
-}
-
-function loop() {
-    if (!stop && !paused) {
-        // draw ;
-        var time= +new Date();
-        if (time > (tick + prevTime)) {
-            $('#time').html("time: " + time + ", prevTime: " + prevTime);
-            readInput();
-        //logic
-        //etc
-            prevTime= time;
-        }
-    } else if (pause) {
-        pause();
-    } else {
-        return;
-    }
-    /*
-    if (++runs >= maxruns) {
-        window.clearInterval(interval);
-    }
-    */
-}
 
 function pause() {
     window.clearInterval(interval);
-    msg('pause');
+    $('#paused').html('paused');
     return;
 }
 
 function readInput() {
-    var i= 0;
-    for (i= 0; i < down.length; i++)
-        if (!$.inArray(down[i], keys)) {
-            keys[length]= down[i];
-            
-            if (down[i] == 27)
-                pause= true;
-        }
-            
-    for (i= 0; i < up.length; i++)
-        if ($.inArray(up[i], keys))
-            keys.splice(keys.indexOf(up[i]), 1);
-    
-    var html= "";
+    var html= "keys length: " + keys.length + "<br />";
     for(var i= 0; i < keys.length; i++) {
-        if (keys[i] == '27')
+        if (keys[i] == '80')
             pause= true;
         html = html + "key true:&nbsp" + keys[i] + "<br />";
     }
-    $('#keys').html(html);
+    if (debug)
+        $('#keys').html(html);
+}
+
+function init() {
+    $(window).keydown(function(event) {
+        if ($.inArray(event.keyCode, keys) == -1)
+            keys[keys.length]= event.keyCode;
+    });
+    
+    $(window).keyup(function(event) {;
+        var index= $.inArray(event.keyCode, keys);
+        if (index != -1)
+            keys.splice(index, 1);
+        else
+            keys= [];
+    });
+
+    drawObject = { "x": "0", "y": "0", "color": "blue"};
+    tick= 50;
+    prevTime= +new Date();
+
+    interval= setInterval("loop()", 0);
+}
+
+function loop() {
+    if (paused) {
+        pause();
+        return;
+    }
+    
+    // draw ;
+    var time= +new Date();
+    if (time > (tick + prevTime)) {
+        prevTime= time;
+        if (debug) {
+            $('#time').html("time: " + time + ", prevTime: " + prevTime);
+        }
+        readInput();
+    }
 }
 
 function msg(text) {
-    $('#msg').html(text);    
+    if (debug)
+        $('#msg').html(text + "<br />" + $('#msg').html());    
 }
