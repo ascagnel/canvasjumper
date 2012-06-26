f30e8db69d5d6a0b7502ac1ac65404f0dfa0f721
Andrew Scagnelli <andrew.scagnelli@gmail.com>
2012-06-25 21:34:29 -0400

    Initial checkin.  Got the canvas reading properly (for some reason I can't understand, you need to use those exact variables when getting the canvas & its 2D context).

--- js/canvas.js
+++ js/canvas.js
@@ -1,66 +1,16 @@
-//Canvas.js
-//ascagnel -- Andrew Scagnelli
-
-/*
-[11:51pm] ascagnel: you essentially set a timer to run on one (for server ticks)
-[11:51pm] ascagnel: and the framerate to run unbounded
-*/
-var drawObject= null;
-var paused= false;
-var tick= 0;
-var inputKey= 0;
-var prevTime= 0;
-var keys= [];
-var interval;
-var keyManager;
-
-
-function pause() {
-    window.clearInterval(interval);
-    $('#paused').html('paused');
-    return;
-}
-
-function init() {
-    console.log("Debug level: " + debug);
-    keyManager= new Keys();
-    keyManager.init(debug);
+function Canvas () {
+    this.canvas= null;
     
-    $(window).keydown(function(event) {
-        keyManager.addKey(event.keyCode);
-    });
-    console.log("Key down bound");
-    
-    $(window).keyup(function(event) { 
-        keyManager.clearKey(event.keyCode);
-    });
-    console.log("Key up bound");
-
-    drawObject = { "x": "0", "y": "0", "color": "blue"};
-    tick= 50;
-    prevTime= +new Date();
-
-    interval= setInterval("loop()", 0);
-}
-
-function loop() {
-    if (paused) {
-        pause();
-        return;
+    this.init = function () {
+        var canvas = document.getElementById("canvas");  
+        var ctx = canvas.getContext("2d");  
+        
+        this.canvas= ctx;
+        
+        this.canvas.beginPath();
+        this.canvas.rect(10, 10, 20, 20);
+        this.canvas.closePath();
+        this.canvas.fill();
     }
     
-    // draw ;
-    var time= +new Date();
-    if (time > (tick + prevTime)) {
-        prevTime= time;
-        if (debug) {
-            $('#time').html("time: " + time + ", prevTime: " + prevTime);
-        }
-        keyManager.process();
-    }
-}
-
-function msg(text) {
-    if (debug)
-        $('#msg').html(text + "<br />" + $('#msg').html());    
 }
